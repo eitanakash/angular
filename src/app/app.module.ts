f92331f6f848ea3890420b33e3c1d42ef46c9c5c
eitanakash <eitan.n@trgint.com>
2019-08-27 21:26:05 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { SeverComponent } from './server/server.component';
+import { from } from 'rxjs';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeverComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
